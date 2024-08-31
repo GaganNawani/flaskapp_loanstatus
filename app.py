e87754293136e85bfbf3d0ed490e0286bc9f92d5
Gagan Nawani <gagan.nawani@gmail.com>
2024-08-31 21:54:09 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def welcome():
+    return "<html>WELCOME STRANGER</html>"
 
-@app.route("/ping", methods = ['GET'])
-def pinger():
-    return {'MESSAGE': 'Hello, I am PINGING....'}
+@app.route("/index", methods = ['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route("/about", methods = ['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/form', methods = ['GET','POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+@app.route('/submit', methods = ['GET','POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
